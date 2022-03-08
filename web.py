70ab9af1ed3dd9bc45b812a8c2505c8235fe195d
mannapuder <mindikkaveeleiolesiin@gmail.com>
2022-03-09 00:22:51 +0200

    fix host binding, again

--- web.py
+++ web.py
@@ -45,7 +45,7 @@ def run(queue):
     app.config['work'] = {}
     app.config['queue'] = queue
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
